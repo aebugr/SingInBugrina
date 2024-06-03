785e6d27df303a771bfe877ce8c4decd443ee48b
BugrinaA <tsaaeel@gmail.com>
2024-06-04 01:58:02 +0500

    Разработанный код для переключения страниц. Бугрина

--- app/src/main/java/com/example/singinbugrina/MainActivity.java
+++ app/src/main/java/com/example/singinbugrina/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.singinbugrina;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.MotionEvent;
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.singin);
+    }
+    public int start_x = 0;
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                start_x = (int) event.getX();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                if (Math.abs((int) event.getX() - start_x) > 50) {
+                    if (start_x < (int) event.getX()) {
+                        setContentView(R.layout.singin);
+                    } else {
+                        setContentView(R.layout.regin);
+                    }
+                }
+                break;
+        }
+        return false;
     }
 }
