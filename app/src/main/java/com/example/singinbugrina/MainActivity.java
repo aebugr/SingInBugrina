6180ca21e9a4a823b79f087c03d221916246b490
aebugr <tsaaeel@gmail.com>
2024-09-08 16:35:01 +0500

    Создание класса DataUser. Бугрина

--- app/src/main/java/com/example/singinbugrina/MainActivity.java
+++ app/src/main/java/com/example/singinbugrina/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.singinbugrina;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,4 +34,32 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
+
+    public String login, password;
+
+    public void onAutorization(View view){
+        TextView tv_login = findViewById(R.id.login);
+        login = tv_login.getText().toString();
+
+        TextView tv_password = findViewById(R.id.password);
+        password = tv_password.getText().toString();
+
+        GetDataUser gdu = new GetDataUser();
+        gdu.execute();
+    }
+
+    public class DataUser{
+        public String id;
+        public String login;
+        public String password;
+
+        public void setId(String _id) { this.id = _id; }
+        public String getId() { return this.id; }
+        public void setLogin(String _login) { this.login = _login; }
+        public String getLogin() { return this.login; }
+        public void setPassword(String _password) { this.password = _password; }
+        public String getPassword() {return this.password; }
+    }
+
+    ArrayList<DataUser> dataUser = new ArrayList<>();
 }
