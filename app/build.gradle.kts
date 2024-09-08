46fcfeb524c5e3a92c40e7c27dbd17ee031da531
aebugr <tsaaeel@gmail.com>
2024-09-08 16:32:16 +0500

    Установка библиотеки. Бугрина

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 }
 
 dependencies {
-
+    implementation("org.jsoup:jsoup:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
